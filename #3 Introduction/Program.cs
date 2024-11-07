ae0e9e646baa49cee94ce31290732d10db48a687
adamdross27 <adamross387@gmail.com>
2024-11-07 22:22:52 +1300

    first use of an if statement and string nullables

---
+++ #3 Introduction/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter a number: ");
+string? answer = Console.ReadLine();
+
+if(!string.IsNullOrEmpty(answer))
+{
+    Console.WriteLine("You entered: " + answer);
+}
+else
+{
+    Console.WriteLine("No input was provided.");
+}
